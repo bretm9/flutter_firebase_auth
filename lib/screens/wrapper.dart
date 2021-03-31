cff9cca4d7c0bf8a2adcdf40a1450b456f820347
Bret Merritt <merrittbret9@gmail.com>
2021-03-31 17:16:12 -0600

    Remove Home import

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth/screens/signin/signin.dart';
-import 'package:flutter_firebase_auth/screens/home/home.dart';
 
 class Wrapper extends StatelessWidget {
   @override
