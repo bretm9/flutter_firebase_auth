8ae6070dc6866ebe3431d7b9c6998ba140e85fc5
Bret Merritt <merrittbret9@gmail.com>
2021-03-31 17:04:27 -0600

    Remove Provider Dependency

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth/screens/signin/signin.dart';
 import 'package:flutter_firebase_auth/screens/home/home.dart';
-import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
