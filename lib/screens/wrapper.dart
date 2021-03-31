412315df8960dd8a716e4939e1c2f5699eeb9cc6
Bret Merritt <merrittbret9@gmail.com>
2021-03-31 17:04:03 -0600

    Wrapper returns Signin

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/screens/signin/signin.dart';
 import 'package:flutter_firebase_auth/screens/home/home.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Home();
+    return SignIn();
   }
 }
